ca619c14cd2daaca8b5ed9d2e64be631c4610982
Michael Popovich <earthmabus@hotmail.com>
2025-09-28 11:09:12 -0400

    updated to handle exceptions when communicating with the api servers

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ def is_iss_viewable(iss_lat, iss_long, my_lat, my_long, sunrise_hour, sunset_hou
     '''returns true if the ISS is viewable overhead -- must be dark and within +/- 5 degrees of my location'''
     in_view_range = abs(iss_lat - my_lat) <= 5 and abs(iss_long - my_long) <= 5
     is_dark_outside = sunset_hour < current_hour < sunrise_hour
+    if in_view_range and not is_dark_outside:
+        print("ISS is above, but it's not dark outside")
     return in_view_range and is_dark_outside
 
 # my current location
@@ -36,28 +38,33 @@ while True:
     time_now_hour = time_now.hour
     print(f"{time_now}: checking if ISS is above...")
 
-    # get the current location of the ISS
-    response_iss_loc = requests.get(url="http://api.open-notify.org/iss-now.json")
-    response_iss_loc.raise_for_status()
-    iss_lat = float(response_iss_loc.json()['iss_position']['latitude'])
-    iss_long = float(response_iss_loc.json()['iss_position']['longitude'])
+    try:
+        # get the current location of the ISS
+        response_iss_loc = requests.get(url="http://api.open-notify.org/iss-now.json")
+        response_iss_loc.raise_for_status()
+        iss_lat = float(response_iss_loc.json()['iss_position']['latitude'])
+        iss_long = float(response_iss_loc.json()['iss_position']['longitude'])
 
-    # get the sunrise/sunset for my location
-    parameters = {"lat": LAT_ORLANDO, "lng": LONG_ORLANDO, "formatted": 0}
-    response_sunrise = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-    response_sunrise.raise_for_status()
-    sunrise_sunset_data = response_sunrise.json()
-    time_sunrise_hour = int(sunrise_sunset_data['results']['sunrise'].split("T")[1].split(":")[0])
-    time_sunset_hour = int(sunrise_sunset_data['results']['sunset'].split("T")[1].split(":")[0])
+        # get the sunrise/sunset for my location
+        parameters = {"lat": LAT_ORLANDO, "lng": LONG_ORLANDO, "formatted": 0}
+        response_sunrise = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+        response_sunrise.raise_for_status()
+        sunrise_sunset_data = response_sunrise.json()
+        time_sunrise_hour = int(sunrise_sunset_data['results']['sunrise'].split("T")[1].split(":")[0])
+        time_sunset_hour = int(sunrise_sunset_data['results']['sunset'].split("T")[1].split(":")[0])
 
-    # if the ISS is above and it is dark outside, send email
-    if is_iss_viewable(iss_lat, iss_long, LAT_ORLANDO, LONG_ORLANDO, time_sunset_hour, time_sunset_hour, time_now_hour):
-        message = "The ISS is viewable right now!\n"
-        message += f"- Your location ({LAT_ORLANDO}, {LONG_ORLANDO})\n"
-        message += f"- ISS location ({iss_lat}, {iss_long})\n"
-        message += f"- sunset ({time_sunset_hour}) < current hour ({time_now_hour}) < sunrise ({time_sunrise_hour})\n"
-        print(f"{time_now}: sending email\n{message}")
-        account.send_email("earthmabus@hotmail.com", "The ISS is above you!", message)
+        # if the ISS is above and it is dark outside, send email
+        if is_iss_viewable(iss_lat, iss_long, LAT_ORLANDO, LONG_ORLANDO, time_sunset_hour, time_sunset_hour, time_now_hour):
+            message = "The ISS is viewable right now!\n"
+            message += f"- Your location ({LAT_ORLANDO}, {LONG_ORLANDO})\n"
+            message += f"- ISS location ({iss_lat}, {iss_long})\n"
+            message += f"- sunset ({time_sunset_hour}) < current hour ({time_now_hour}) < sunrise ({time_sunrise_hour})\n"
+            print(f"{time_now}: sending email\n{message}")
+            account.send_email("earthmabus@hotmail.com", "The ISS is above you!", message)
+    except requests.exceptions.ConnectTimeout:
+        print(requests.exceptions.ConnectTimeout)
+    except Exception:
+        print(Exception)
 
     # sleep until the next time to check for the ISS
     time.sleep(POLL_TIME_IN_SECONDS)
